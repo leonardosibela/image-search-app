c0288a7ec520be9ca4e2c999f7bea3655d45554e
leonardosibela <leonardo_sibela@hotmail.com>
2021-03-07 18:17:21 -0300

    Adding gallery fragment class

---
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -0,0 +1,10 @@
+package com.codinginflow.imagesearchapp.ui.gallery
+
+import androidx.fragment.app.Fragment
+import com.codinginflow.imagesearchapp.R
+
+class GalleryFragment : Fragment(R.layout.fragment_gallery) {
+
+
+
+}
