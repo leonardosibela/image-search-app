fc65337c71e150ce45dad0d24b1156f152b93901
leonardosibela <leonardo_sibela@hotmail.com>
2021-03-07 22:24:31 -0300

    Implementing Load State

--- app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/codinginflow/imagesearchapp/ui/gallery/GalleryFragment.kt
@@ -5,8 +5,10 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.View
 import androidx.appcompat.widget.SearchView
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.paging.LoadState
 import com.codinginflow.imagesearchapp.R
 import com.codinginflow.imagesearchapp.databinding.FragmentGalleryBinding
 import dagger.hilt.android.AndroidEntryPoint
@@ -32,10 +34,33 @@ class GalleryFragment : Fragment(R.layout.fragment_gallery) {
                 header = UnplashPhotoLoadStateAdapter { adapter.retry() },
                 footer = UnplashPhotoLoadStateAdapter { adapter.retry() }
             )
+
+            buttonRetry.setOnClickListener {
+                adapter.retry()
+            }
         }
 
         viewModel.photos.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+
+        adapter.addLoadStateListener { loadState ->
+            binding.apply {
+                progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+                recyclerView.isVisible = loadState.source.refresh is LoadState.NotLoading
+                buttonRetry.isVisible = loadState.source.refresh is LoadState.Error
+                textViewError.isVisible = loadState.source.refresh is LoadState.Error
+
+                // empty view
+                if (loadState.source.refresh is LoadState.NotLoading &&
+                        loadState.append.endOfPaginationReached &&
+                        adapter.itemCount < 1) {
+                    recyclerView.isVisible = false
+                    textViewEmpty.isVisible = true
+                } else {
+                    textViewEmpty.isVisible = false
+                }
+            }
         }
 
         setHasOptionsMenu(true)
